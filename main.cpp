2c9fc0b1fcea648aa50408f10d76b832f166d397
YakymivDanylo <dr.yakimiv@gmail.com>
2024-05-05 15:40:20 +0300

    task 2 and 3 done

--- main.cpp
+++ main.cpp
@@ -1,11 +1,65 @@
 #include <algorithm>
 #include <iostream>
 #include <set>
+#include <vector>
 #include <string>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+/*vector<int> firstFunc(const vector<int> &mas1, int &counter) {
+    vector<int> sorted = mas1;
+    sort(sorted.begin(), sorted.end());
+    if(counter>0)
+        counter--;
+    return sorted;
+}
+void secondFunc(const vector<int> &mas1, int &counter, int cooldown) {
+    if(counter == 0) {
+        vector<int> res = mas1;
+        int sum = 0;
+        for (auto it = res.begin(); it != res.end(); it++) {
+            sum += *it;
+        }
+        cout<<endl;
+        cout << sum;
+        counter += cooldown;
+    }
+    else
+        cout<<"There is cooldown";
+}
+vector<int> thirdFunc(const vector<int> &mas1, int &counter){
+    vector<int> res = mas1;
+    for(auto it = res.begin(); it != res.end(); it++){
+        *it *= *it;
+    }
+    if(counter>0)
+        counter--;
+    return res;
+}*/
+
+
+
+
+
+
+/*//task3
+ struct Player{
+    string name;
+    int score;
+};
+
+void Print(const vector<Player> &player){
+    cout<<"---------Player rating---------"<<endl;
+    for(int i=0;i<player.size();i++){
+        cout << i + 1 << ". " << player[i].name << ": " << player[i].score << endl;
+    }
+}*/
+
+
 int main() {
+/* //task 1
     string str1 = "abracadabra";
     string str2 = "dab";
 
@@ -23,6 +77,40 @@ int main() {
     } else {
         cout << "there is no range with symbol from str2" << endl;
     }
+*/
+/*//task 2
+
+        vector<int> mas1{1,3,2,4,5,9,6};
+        int cooldown;
+        cout<<"Please write your cooldown";
+        cin>>cooldown;
+        int counter=0;
+        vector<int> result = firstFunc(mas1, counter);
+        for_each(result.begin(),result.end(),
+                 [](int &x){cout<<x<<" ";});
+        secondFunc(mas1,counter,cooldown);
+        cout<<endl;
+        vector<int> res = thirdFunc(mas1, counter);
+        for_each(res.begin(),res.end(),
+                 [](int &x){cout<<x<<" ";});
+        cout<<endl;
+        secondFunc(mas1,counter,cooldown);
+*/
+
+/*//task 3
+    vector<Player> players = {
+            {"Alice", 1000},
+            {"Bob", 800},
+            {"Charlie", 1200},
+            {"David", 900},
+            {"Emily", 700},
+    };
+    sort(players.begin(),players.end(),[](const Player &p1, const Player &p2){
+        return p1.score>p2.score;
+    });
+    Print(players);*/
+
+
 
     return 0;
 }
